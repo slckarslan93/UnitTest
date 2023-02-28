82a62adf21a83c0cbc593178daad467beaf6ab5b
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-02-28 09:21:34 +0300

    Add project files.

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using UnitTest.APP;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+            //Act
+            var total = calculator.add(a, b);
+
+            //Assert
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
